9e4252175626b62559fd63b644771ab788d7f6be
LakshmanTurlapati <lakshmantvnm@gmail.com>
2025-03-26 18:35:17 -0500

    Commit: About Us - Implementations Checkpoint

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './About.css';
 import logo from '../assets/awsxutd.png';
+import advisorImage from '../assets/officers/advisor.jpeg';
 
 function About() {
   return (
@@ -12,6 +13,39 @@ function About() {
         <a href="#careers" className="nav-item">Careers</a>
         <Link to="/join" className="nav-item join-button">Join Us</Link>
       </nav>
+      
+      <div className="vertical-line"></div>
+      
+      <div className="advisor-section">
+        <div>
+          <div className="advisor-image">
+            <img src={advisorImage} alt="Prof. Engin Calisir" />
+          </div>
+          <div className="advisor-name">
+            Prof. <strong>Engin Calisir</strong>
+            <div>Advisor</div>
+          </div>
+        </div>
+        <div className="advisor-info">
+          <p>
+            A results-oriented senior IT executive with over 20 years of experience in information technology in higher education and research. Prof Engin is a professor in the Information Systems department and Assistant Dean, Technology and Facilities Services.
+          </p>
+        </div>
+      </div>
+      
+      <div className="about-content">
+        <h2>About Us</h2>
+        <p>
+          Welcome to the AWS Organization at The University of Texas at Dallas—a vibrant community dedicated to exploring the forefront of AWS cloud computing. We're all about igniting curiosity and fueling growth, offering workshops, mock interviews, certification paths, and Q&A sessions led by seasoned AWS experts. Whether you're just starting out or already deep in the tech trenches, our events are designed to broaden your horizons and sharpen your skills in cloud technology.
+        </p>
+        <p>
+          Join us to tap into a network of passionate IT and engineering students, where learning meets opportunity. Here, you'll not only dive into the latest AWS innovations but also build connections with industry professionals, paving the way for both personal and professional success. Come be a part of our journey and experience firsthand how AWS can transform your academic and career paths at UTD!
+        </p>
+      </div>
+      
+      <div className="our-team-heading">
+        <h2>Our Team</h2>
+      </div>
       
       <div className="logo-container">
         <img src={logo} alt="AWS Logo" className="about-logo" />
